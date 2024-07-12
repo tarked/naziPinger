56e4567351dcda26d3130f17b575facec9d4450b
tarked <zayfnmail@gmail.com>
2024-07-11 21:17:01 -0400

    Update NoSmoke.bat

--- NoSmoke.bat
+++ NoSmoke.bat
@@ -1,59 +1,64 @@
 @echo off
 color 6
 
-title NOSMOKING
+title 
 
-:greeting
+:greeting 
 cls
-echo 
-echo ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-echo░░░░░████░░░░░░░░░░░░░░░████░░░░░
-echo░░░░███░░░░░░░░░░░░░░░░░░░███░░░░
-echo ░░░███░░░░░░░░░░░░░░░░░░░░░███░░░
-echo ░░███░░░░░░░░░░░░░░░░░░░░░░░███░░
-echo ░███░░░░░░░░░░░░░░░░░░░░░░░░░███░
-echo ████░░░░░░░░░░░░░░░░░░░░░░░░░████
-echo ████░░░░░░░░░░░░░░░░░░░░░░░░░████
-echo ██████░░░░░░░███████░░░░░░░██████
-echo █████████████████████████████████
-echo █████████████████████████████████
-echo ░███████████████████████████████░
-echo ░░████░███████████████████░████░░
-echo ░░░░░░░███▀███████████▀███░░░░░░░
-echo░░░░░░████──▀███████▀──████░░░░░░
-echo ░░░░░░█████───█████───█████░░░░░░
-echo ░░░░░░███████▄█████▄███████░░░░░░
-echo ░░░░░░░███████████████████░░░░░░░
-echo ░░░░░░░░█████████████████░░░░░░░░
-echo ░░░░░░░░░███████████████░░░░░░░░░
-echo ░░░░░░░░░░█████████████░░░░░░░░░░
-echo░░░░░░░░░░░███████████░░░░░░░░░░░
-echo ░░░░░░░░░░███──▀▀▀──███░░░░░░░░░░
-echo ░░░░░░░░░░███─█████─███░░░░░░░░░░
-echo  ░░░░░░░░░░░███─███─███░░░░░░░░░░░
-echo ░░░░░░░░░░░░█████████░░░░░░░░░░░░
-echo ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+echo                 .'.:l.  ..                                                                                                  .oc..dk.                
+echo               .cOdcxKo.cKk.                                                                                         ..  .::.oNOd:cl.                
+echo                .o0oxXXd'dWNc..,,.  .c:.      ..'       ,'                                                .    ;x:   :k,.;cx0,;XXXd;'                 
+echo                  .c:dWWd;kWMx:okKd'cKWWk'   'd0N0,    .oX0,      .:,                           ..       .cl. .,xWNd.,0Xc.cd0X:.kMNl.                  
+echo                   .,0Wd;OMWxcONM0o0MMMW0; .OWMMXo:. .oWMMO'    .oXl              .l;        .cd.   ',.:ONo  :OXMM0;dWNx,'xNWl.kWd.                   
+echo                     cXO;xMNc;KMM0dKMMMMWd'dWMMMWNNk..xMMMK,   .oNMx..'          .:Kd.      'xNd. .lK0ONMK:..kWMMMK;lNMK; oWX:.xO'                    
+echo                      :kccXX:,KMMKkXMMMWNo'kMMMMMMMWo.dWMMO::' :XMM0,lO;        lkkNd,'   .cKWX:  ;XMMMMMkc.,KMMMMK;;KMK, cNx..l;                     
+echo                       ';.cKo;0MM0xXMMMXOc.oWMMMMMMX:.oWMWkxXd.,KMMXcoWK:     .lXWWWdc: .:kNMM0c. .kMMMMNkc..kMMMMX:.OMO. lO,  .                      
+echo                           :o'lNWo'xWMM0o, ,KMMMMMXc  oWMWKXMX:.kMMWddWMXl. .c0WMMMNdxc.cNMMMMXO;  :XMMMKkl. cNMMMN:'OK;  ::                          
+echo                            ...OK, ,KMMO:. .xMMMMNl   lWMMWMMMx'oWMMOkWMMX: .cXMMMMXO0l .kWMMMMK;  .xWMMX0c  ;KMMNd.,kc   ..                          
+echo                              ;l.  lNM0,   cNMMWx.   lWMMMMMK:.:XMMXXWMMO'   cXMMMWNNl  '0MMMM0'   lWMMWK;  cNMXl. ,:.                               
+echo                                .   .xWNl   .kWMMx.   lWMMMWO,  '0MMMMMMX:     cXMMMMNl   ;KMMMk.   oWMMMk. .dWK:                                     
+echo                                     .dXO.   .xNM0,   :XMMM0'   .xMMMMMNo.      lNMMMNc   .dWMX:   .kMMWO,  .O0,                                      
+echo                                       ;x:    .lXNl   .dNMMk.    cNMMMMO'       ,0MMWx.    lWWd.   ,KMXo.   ,o'                                       
+echo                                        .'.     :Kx.   .xWMk.    .xWMMMk.       ,KMM0'     lN0'    cXO;     ..                                        
+echo                                                 ,o'    .kWO.     .OMMMk.       ;KMK;      lKc    .co.                      .                         
+echo                         .;c.                     ..     'Ok.      ;KMMx.       ;XNl       co.     .                       .lkc.                      
+echo                        .xNx.  ..                         ,l.       cNMx.       :Kd.       ,'                           ;;. ;KWl                      
+echo                        ,KX:.,dc.                          .        .dNx.       :d.                                  .  .xO:'xNl                      
+echo                        .dxcxXk.  ,.                                 .kd.       ''                                  .:,  cNWxlc.                      
+echo                         .'oNWx..;o'                                  ,:.                                            ox;'lXMx.                        
+echo                           ;XWxcoOk.  ..                               .                             ,'         .:'  cX0doKWl                         
+echo                          .dOoxXWx. :x,       ..            ;;                                      lk,         oO:.:NMXl:c.                         
+dchi                             .,OMWo'dXd.      .c;           :Oc                                      ;XMK:  .:' .:kWXlddWO. 
+echo                              .dWKldWXl. .,.  .ko          ;KX;                                ..    ;XMK:  .:' .:kWXldWO.                            
+echo                               ;0dlXW0x: ;o.  :XO.        ;KM0'         ,;              .:.    ;:    '0MMXc .dd.'x0NMOcl;                             
+echo                                .'xWWNKc.dk.  oWX;       ,0MMO.         lk'             ;O:    lk'   .OMMM0'.xXl;OWWM0,                               
+echo                                  :XMMO:lK0' .OMWo    .'.lWMMk.        .xNo.           .oNx.   lXo   .OMMMX;.xWXddXMXc                                
+echo                                   c0OccKWNl ;XMMO.   .:;oWMWd.        '0M0d,        .',kMO.   oW0,  ,KMMMNc;KMMNdcl,                                 
+echo                                    ...xWMMO,oWMMK;   .lodWMMO'        :XMWNd.       ;llXMO.  .dWWd. :NMMMWo;0MMMk.                                   
+echo                                    .OMMMO,c0XMWo    dOOWMMWd.       oWMMMK;      .xkkWMO.  .xMMK;.oWMMMWd'kMMXl.                                   
+echo                                        ;OWMO''l0MMK;   dNXWMMMNc      '0MMMMWx.     cXKKMMO.  .kMMWo.xMWMMMk,dW0;                                     
+echo                                         .l0d..,kMMMKc  dWMMMMMM0,    .oWMMMMMX:    'OMWWMMO. .lXMMMx:0N0XMMO'.;.                                      
+echo                                          .   .dWMMMNd'dMMMMMMMWx.   ;KMMMWXWMk.  .xWMMMMMO''xNMWWMOl0OxNMM0'                                         
+echo                                               oWMMMWx,dWMMMMMMMX;  .xWMMW0kKMX; .xWMMMMMMO:kMMN0KM0:ccdWW0:                                          
+echo                                               'xXWWx. :XMMMMMMMNc  cXMMMWOd0MX;.xWMMMMMMMO:kMNkkNMO'  'lc.                                           
+echo                                                 'cc.   cXMMMMMMWo .OMMMMXkd0MK,'0MMMMMMMMk,xNdlKW0;                                                  
+echo                                                         ;OWMMMMMx.;XMMMM0oo0M0,;XMMMMMMMK; ':;kNk'                                                   
+echo                                                          .c0WMWNx.;XMMMMkco0MO'lNMMMMMW0;    .:;.                                                    
+echo                                                             .:l:'. 'OWMMWd:o0Mk'oWMMMMXo.                                                             
+echo                                                              .;dKXc,okx,.;k0NXx'                                           
 echo                          
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000 
 echo 
 eecho 88                    88  
 echo  88                    88  
 echo  88                    88  
 echo  88 ,adPPYba,  ,adPPYb,88  
 echo  88 I8[    "" a8"    `Y88  
-echo  88  `"Y8ba,  8b       88  map
+echo  88  `"Y8ba,  8b       88  MAP
 echo  88 aa    ]8I "8a,   ,d88  
 echo  88 `"YbbdP"'  `"8bbdP"Y8 
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000
 set /p Ip=
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=0b & echo ───▄▄▄
-ech o─▄▀░▄░▀▄
-echo ─█░█▄▀░█
-echo ─█░▀▄▄▀█▄█▄▀
-echo ▄▄█▄▄▄▄███▀)
+IF ERRORLEVEL 1 (SET in=0b & echo downed)
 set /a num=(%Random%%%9)+1
 color %num%
 ping -t 1 0 10 127.0.0.1 >nul
